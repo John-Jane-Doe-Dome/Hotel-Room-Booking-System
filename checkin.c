639e9e92f4ba6a7328a7800689b136bb076fe069
sothykolreak-cell <sothykolreak@gmail.com>
2026-06-24 16:05:46 +0700

    Progress so far

--- checkin.c
+++ checkin.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "viewreserveroom.h"
+// finds the room
+void check_in(struct Room rooms[], int total)
+{
+    int numbers;
+    int position;
+    printf("Enter the number of the room you have booked: ");
+    scanf("%d", &numbers);
+    while (getchar() != '\n')
+        ;
 
+    position = find_room(rooms, total, numbers);
+
+    // check if the room exist or not
+    if (position == -1)
+    {
+        printf("Room %d does not exist.\n", numbers);
+        return;
+    }
+
+    // check if the room has been booked or not
+    if (rooms[position].status != RESERVED)
+    {
+        printf("Room %d has not been booked.\n", numbers);
+        return;
+    }
+
+    // update the information
+    rooms[position].status = OCCUPIED;
+
+    // tell the user they have been successfully checked-in
+    printf("==============================\n");
+    printf("   Check-in successful!\n");
+    printf("==============================\n");
+    printf("Guest: %s\n", rooms[position].guest);
+    printf("Tel: %s\n", rooms[position].phone);
+    printf("Email: %s\n", rooms[position].email);
+    printf("Floor: %d\n", rooms[position].floors);
+    printf("Room: %d (%s)\n", rooms[position].numbers, rooms[position].type);
+    printf("Night: %d\n", rooms[position].nights);
+    printf("==============================\n");
+    printf("Please enjoy your stay!\n\n");
+}
--- main.c
+++ main.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
+#include "register.h"
+#include "viewreserveroom.h"
+int main()
+{
+    // storage
 
-int main(){
+    struct Room rooms[MAX_ROOMS];
+    int total = 0;
 
+    // customer information
+
+    char CustomerName[100];
+    char PhoneNumber[20];
+    char CustomerEmail[100];
+
+    // creates the room
+    init_rooms(rooms, &total);
+
+    // register the customer information
+    registration(CustomerName, PhoneNumber, CustomerEmail);
+
+    // Function Menu
+    printf("\n==============================\n");
+    printf("Welcome, %s \n", CustomerName);
+    printf("How can we help you today?\n");
+    printf("1. View rooms\n2. Book rooms\n3. View Booking\n4. Check-in\n5. Check-out\n6. Exit");
+    printf("\n==============================\n");
+
+    // Menu
+    int choice;
+    char type[15];
+    do
+    {
+        // for user to pick an action that they want to do
+        printf("Please pick an action (1-6): ");
+        scanf("%d", &choice);
+
+        // ← flush leftover newline
+        while (getchar() != '\n')
+            ;
+        switch (choice)
+        {
+        case 1:
+            display_all_rooms(rooms, total);
+            break;
+        case 2:
+            book_room(rooms, total, CustomerName, PhoneNumber, CustomerEmail);
+            break;
+        case 3:
+            break;
+            check_in(rooms, total);
+        case 4:
+            break;
+        case 5:
+            check_out(rooms, total);
+            break;
+        case 6:
+            printf("Thank you %s!", CustomerName);
+            break;
+        default:
+            printf("Invalid choice.\n");
+            printf("1. View rooms\n2. Book rooms\n3. View Booking\n4. Check-in\n5. Check-out\n6. Exit");
+            break;
+        }
+    } while (choice != 6);
     return 0;
 }
